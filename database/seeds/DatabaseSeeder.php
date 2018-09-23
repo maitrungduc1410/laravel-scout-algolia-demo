5780ed234c66e38c61742398cb471a6065b78bd3
Mai Trung Duc <maitrungduc1410@gmail.com>
2018-09-23 19:06:50 +0800

    update

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        factory(User::class, 7500)->create();
+        // factory(User::class, 7500)->create();
+        for ($i=0; $i < 1000; $i++) { 
+	         DB::table('users')->insert([
+	            'name' => str_random(3).' '.str_random(3).' '.str_random(3),
+		        'email' => str_random(3).' '.str_random(3).' '.str_random(3).'@gmail.com',
+		        'email_verified_at' => now(),
+		        'password' => bcrypt('123456'), // secret
+		        'remember_token' => str_random(10),
+	        ]);
+	    }
     }
 }
